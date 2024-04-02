a471b44568123fc760d801ec1b543f7d5d51f525
zleyyij <zleyyij@noreply.users.github.com>
2024-04-02 12:18:23 -0600

    feat: configure logging level with cli arg

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ use axum::http::{HeaderValue, StatusCode};
 use axum::routing::post;
 use axum::{extract::State, routing::get, Json, Router};
 use chrono::Local;
-use clap::Parser;
+use clap::builder::TypedValueParser;
+use clap::{Parser, ValueEnum};
 use colored::*;
 use cpu::{Cpu, CpuCache};
+use http::{header, Method};
 use log::{error, info, warn};
 use log::{Level, LevelFilter, Metadata, Record};
 use pcie::PcieCache;
@@ -17,7 +19,6 @@ use serde::{Deserialize, Serialize};
 use std::env;
 use tower_http::cors::CorsLayer;
 use usb::UsbCache;
-use http::{Method,header};
 
 /// Because the error that nom uses is rather lengthy and unintuitive, it's defined here
 /// to simplify handling
@@ -53,10 +54,29 @@ impl log::Log for SimpleLogger {
     fn flush(&self) {}
 }
 
+#[derive(ValueEnum, Clone)]
+enum LoggingLevel {
+    Silent,
+    Error,
+    Warning,
+    Info,
+    Debug,
+    Trace,
+}
+
 #[derive(Parser)]
 struct Args {
-    #[arg(short = 'p', long = "port")]
-    port: Option<String>,
+    /// Set the port to listen on
+    #[arg(short = 'p', long = "port", default_value_t = String::from("3000"))]
+    port: String,
+    /// Level of logging verbosity
+    #[arg(short = 'v',
+        long = "verbosity",
+        default_value_t = LevelFilter::Info,
+        value_parser = clap::builder::PossibleValuesParser::new(["trace", "debug", "info", "warn", "error", "off"])
+            .map(|s| s.parse::<LevelFilter>().unwrap())
+        )]
+    logging_level: LevelFilter,
 }
 
 static LOGGER: SimpleLogger = SimpleLogger;
@@ -209,10 +229,10 @@ async fn get_cpu_handler<'a>(
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // initialize logging
-    log::set_logger(&LOGGER)
-        .map(|()| log::set_max_level(LevelFilter::Info))
-        .unwrap();
     let cli_args = Args::parse();
+    log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(cli_args.logging_level))
+        .unwrap();
     info!("Application started");
     // parse command line arguments
     // create a new http router and register respective routes and handlers
@@ -222,17 +242,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/api/usbs/", post(post_usbs_handler))
         .route("/api/pcie/", get(get_pcie_handler))
         .route("/api/pcie/", post(post_pcie_handler))
-        .layer(CorsLayer::new().allow_methods([Method::GET, Method::POST]).allow_headers([header::ACCEPT, header::CONTENT_TYPE]).allow_origin("*".parse::<HeaderValue>().unwrap()))
+        .layer(
+            CorsLayer::new()
+                .allow_methods([Method::GET, Method::POST])
+                .allow_headers([header::ACCEPT, header::CONTENT_TYPE])
+                .allow_origin("*".parse::<HeaderValue>().unwrap()),
+        )
         .with_state(AppState {
             cpu_cache: CpuCache::new(),
             usb_cache: UsbCache::new(),
             pcie_cache: PcieCache::new(),
         });
 
-    let mut port: String = String::from("3000");
+    let mut port: String = cli_args.port;
     if let Ok(value) = env::var("HWAPI_PORT") {
-        port = value;
-    } else if let Some(value) = cli_args.port {
         port = value;
     }
 
